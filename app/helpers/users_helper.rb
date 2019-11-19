fd8c5618f70eb554bb8d0c6d38e1055c9c6e30d5
EC2 Default User <ec2-user@ip-172-31-87-27.ec2.internal>
2019-11-19 01:38:56 +0000

    user sign up

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+    
+  # 引数で与えられたユーザーのGravatar画像を返す
+  def gravatar_url(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+  end
+end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  # test "should get index" do
+  #   get users_path
+  #   assert_response :success
+  # end
+
+  # test "should get show" do
+  #   get user_path(@user)
+  #   assert_response :success
+  # end
+
+  # test "should get new" do
+  #   get new_user_path
+  #   assert_response :success
+  # end
+
+
+end
