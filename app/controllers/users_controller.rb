6007e057ebf1d623ca514091855dcf35c009d5be
EC2 Default User <ec2-user@ip-172-31-87-27.ec2.internal>
2019-11-21 07:11:50 +0000

    user-edit,update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
+  
   def index
     @users = User.order(id: :desc).page(params[:page]).per(10)
   end
@@ -23,9 +26,40 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = '更新しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = '更新に失敗しました。'
+      render 'edit'
+    end
+  end
+
+  
   private
   #ストロングパラメーター
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
